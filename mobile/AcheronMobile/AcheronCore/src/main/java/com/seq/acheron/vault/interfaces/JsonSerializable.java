eef42fe9f628e11e70743bd036934f6048f73bc2
Gabriel Musteata <gmiganescu@gmail.com>
2026-06-28 14:59:36 +0200

    Remove mobile/AcheronMobile, split into SeQ-AcheronMobile repo
    History preserved at https://github.com/gamustea/SeQ-AcheronMobile
    via git subtree split.

--- mobile/AcheronMobile/AcheronCore/src/main/java/com/seq/acheron/vault/interfaces/JsonSerializable.java
+++
@@ -1,9 +0,0 @@
-package com.seq.acheron.vault.interfaces;
-
-import com.seq.acheron.vault.storables.VaultObject;
-
-import java.security.GeneralSecurityException;
-
-public interface JsonSerializable {
-    String toJson() throws GeneralSecurityException;
-}
