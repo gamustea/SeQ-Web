e11fb066c04d86590968a07bde34b0d9fe14f4c7
Gabriel Musteata <gmiganescu@gmail.com>
2026-06-28 17:08:55 +0200

    refactor: removed acheron files (moved to new repo)

--- mobile/AcheronMobile/AcheronCore/src/main/java/com/seq/acheron/vault/interfaces/JsonSerializable.java
+++
@@ -1,7 +0,0 @@
-package com.seq.acheron.vault.interfaces;
-
-import java.security.GeneralSecurityException;
-
-public interface JsonSerializable {
-    String toJson() throws GeneralSecurityException;
-}
